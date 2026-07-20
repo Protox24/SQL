0b25014efcae645be52ebbfeeb050b3c59d97a89
Hector Sánchez <hsanchezg2004@gmail.com>
2026-07-20 15:55:26 -0600

    push

--- sql_project/2_top_paying_jobs_skills.sql
+++ sql_project/2_top_paying_jobs_skills.sql
@@ -1,23 +1,41 @@
 
+/*
+Question: What are the skills that are required for the top-paying data analyst jobs?
+*/
 
-SELECT  
-    job_id,
-    job_title,
-    name as company_name,
-    job_location,
-    job_schedule_type,
-    salary_year_avg,
-    job_posted_date
+with top_skills as (
+    SELECT  
+        job_id,
+        job_title,
+        name as company_name,
+        job_posted_date
 
+    FROM
+        job_postings_fact as jp
+
+    left JOIN company_dim as cd
+    on cd.company_id = jp.company_id
+    WHERE
+        job_location = 'Anywhere' and
+        salary_year_avg is not NULL and
+        job_title_short ='Data Analyst'
+    order BY    
+        salary_year_avg desc
+    limit 10
+)
+
+SELECT
+    skills,
+    count(*) as no_total
 FROM
-    job_postings_fact as jp
+    top_skills as ts
 
-left JOIN company_dim as cd
-on cd.company_id = jp.company_id
-WHERE
-    job_location = 'Anywhere' and
-    salary_year_avg is not NULL and
-    job_title_short ='Data Analyst'
-order BY    
-    salary_year_avg desc
-limit 10;
+inner JOIN skills_job_dim as sjd
+on sjd.job_id = ts.job_id
+left JOIN skills_dim as sd 
+ON sd.skill_id = sd.skill_id
+
+group BY
+    skills
+order BY
+    no_total desc
